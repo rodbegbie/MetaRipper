065e6c9231b118ea90bfc5830f7eb55d6f09fffb
Rod Begbie <rod@arsecandle.org>
2005-08-16 04:47:39 +0000

    Abort on Skip
    git-svn-id: file:///home/rod/meta/filtered@25 2fc86ef6-2fd0-0310-8665-e8bd34009708

--- MetaRipper/Util/RipTrack.py
+++ MetaRipper/Util/RipTrack.py
@@ -14,6 +14,7 @@ def ripTrack(device, trackNo, filename, callbackProgress, callbackComplete):
     cdp = gst.element_factory_make("cdparanoia", "ripper")
     cdp.set_property("device", device)
     cdp.set_property("paranoia-mode", 4)
+    cdp.set_property("abort-on-skip", True)
     track_format = gst.format_get_by_nick("track")
     src_pad = cdp.get_pad("src")
 
