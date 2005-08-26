337e765623208f631bfb8d7f448e9af23a101f3b
Rod Begbie <rod@arsecandle.org>
2005-08-26 02:58:07 +0000

    workable converter.
    git-svn-id: file:///home/rod/meta/filtered@28 2fc86ef6-2fd0-0310-8665-e8bd34009708

--- MetaRipper/Util/Convert.py
+++ MetaRipper/Util/Convert.py
@@ -1,20 +1,24 @@
 import gst
+import os
 
 def error_cb(bin, element, error, debug):
     print error
     raise SystemExit
 
-def convert():
+def convert(flacfile, mp3file):
+    print "Converting %s... " % flacfile
     src = gst.element_factory_make("filesrc", "src")
-    src.set_property("location", "/home/rod/01.flac")
+    src.set_property("location", flacfile)
     src_pad = src.get_pad("src")
 
     flac = gst.element_factory_make("flacdec", "decoder")
        
     mp3 = gst.element_factory_make("lame", "encoder")
+    mp3.set_property("bitrate", 192)
+    mp3.set_property("quality", 2)
     
     sink = gst.element_factory_make("filesink", "sink")
-    sink.set_property("location", "/home/rod/test.mp3")
+    sink.set_property("location", mp3file)
 
     bin = gst.Pipeline()
     bin.add_many(src,flac,mp3,sink)
@@ -26,6 +30,17 @@ def convert():
     res = bin.set_state(gst.STATE_PLAYING);
     while bin.iterate():
         pass
+    print "Done.\n"
     
 if __name__ == "__main__":
-    convert()
+    for root, dirs, files in os.walk("/home/rod/flac"):
+        mp3dir = root.replace("/flac/", "/mp3/")
+        if not os.path.exists(mp3dir):
+            os.makedirs(mp3dir)
+        for file in files:
+            if file.endswith(".flac"):
+                flacfile = os.path.join(root,file)
+                mp3file = flacfile.replace(".flac", ".mp3").replace("/flac/", "/mp3/")
+                #print "Woudl convert %s to %s" %  (flacfile, mp3file)
+                convert(flacfile,mp3file)
+    
